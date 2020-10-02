f7170e31a04ccb0b7b9c8567404eda665e600404
alsov <alsov@w10-noute>
2020-10-03 01:20:15 +0300

    Add: Time view human in model admins

--- mikrotik/models.py
+++ mikrotik/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+from hurry.filesize import size, si, alternative, verbose, traditional
 # Create your models here.
 
 
@@ -7,4 +8,12 @@ class MetricMikrotik(models.Model):
     host = models.CharField(max_length=150)
     traff_uploads = models.IntegerField()
     traff_download = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
+   # date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "Host Name: " + self.host + ", " \
+               + "Date: " + str(self.date.astimezone().strftime("%b %d %Y %H:%M:%S")) \
+               + ", DOWN: " + size(self.traff_download, system=si) \
+               + ", UP: " + size(self.traff_uploads , system=si)
+
